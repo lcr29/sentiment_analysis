7ec929cab4416b1850cb884a9623827d292c01bd
Luis Campero <144020199+lcr29@users.noreply.github.com>
2024-03-04 18:50:42 +0100

    main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import home, choose, analyse, single_song, user_manual
 
 def main():
     st.sidebar.title("Navigation")
-    page = st.sidebar.selectbox("Choose a page", ["Home", "Choose", "Analyse", "Single Song", "User Manual"])
+    page = st.sidebar.selectbox("Choose a page", ["Home", "Choose", "Analyze", "Single Song", "User Manual"])
 
     if page == "Home":
         home.show()
     elif page == "Choose":
         choose.show()
-    elif page == "Analyse":
+    elif page == "Analyze":
         analyse.show()
     elif page == "Single Song":
         single_song.show()
