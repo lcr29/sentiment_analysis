d27795200367e6b77acff21afc0bc16cf57cbb0c
Luis Campero <144020199+lcr29@users.noreply.github.com>
2024-03-04 12:34:26 +0100

    home.py

--- home.py
+++ home.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
 def show():
-    st.title("Welcome to the Music Analysis App")
-    st.write("Use the sidebar to navigate through the application.")
+    st.markdown("""
+        <h1 style='color: #1ed760'>Welcome to SENTUNE! 🎶</h1>
+        <div>Use the sidebar according to your requirements:</div>
+        <ul>
+            <li>Choose: find a playlist that identifies with your desired audience.</li>
+            <li>Analyse: obtain insights from a particular playlist.</li>
+            <li>Single Song: obtain insights from a particular song.</li>
+            <li>User Manual: read our manual if you require help in understanding or using the app interface.</li>
+        </ul>
+        """, unsafe_allow_html=True)
